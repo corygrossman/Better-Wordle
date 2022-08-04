84ea38ff564582a16ee9cdae338daef19dcb902c
corygrossman <cory.grossman@ufl.edu>
2022-08-03 22:44:35 -0400

    Initializing Files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<div class="app">
+<Navbar />
+<router-view />
+</div>
 </template>
 
+<script>
+import Navbar from './components/navbar.vue';
+
+export default {
+  name: 'App',
+  components: { Navbar },
+  date(){
+    return {
+      lightMode: true
+    }
+  },
+  methods: {
+    toggleLightMode(){
+      this.lightMode = !this.lightMode;
+    }
+  }
+
+}
+</script>
+
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app{
+  min-height: 100vh;
+  position: relative;
+  background-color: #222222;
 }
 </style>
