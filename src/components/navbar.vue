3466502ff14e80f5618c51a7542dab4b278ee1b4
corygrossman <cory.grossman@ufl.edu>
2022-08-04 02:54:12 -0400

    Added a functional slider and style changes

---
+++ src/components/navbar.vue
@@ -0,0 +1,71 @@
+<template>
+<nav>
+    <div class="navbar">
+        <div class="logo">
+            <h2>wordle</h2>
+        </div>
+        <div class="bool">
+            <ToggleButton id="lightMode" :defaultState="true" />
+        </div>
+    </div>
+</nav>
+</template>
+
+<script>
+import ToggleButton from './ToggleButton.vue';
+
+export default {
+    components: {
+        ToggleButton
+    },
+
+    data() {
+    return {
+      active: false
+    }
+  },
+  methods: {
+    triggerEvent(value) {
+      this.active = value;
+    }
+  }
+}
+</script>
+
+<style>
+.navbar{
+    background: #222222;
+    color:#f8f8f8;
+}
+.navbar h2{
+    font-style: italic;
+    padding: 24px 24px;
+}
+.toggle_container {
+    margin: 0px auto;
+    background: #efefef;
+    padding: 10px 0;
+    border-radius: 30px;
+    transition: all .25s;
+}
+.toggle_container.active {
+    background: #e9ffef;
+}
+.bool{
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    right: 24px;
+    height: 100%;
+}
+.logo{
+    display: flex;
+    align-items: center;
+}
+nav{
+    display: flex;
+    flex-direction: row;
+    position: relative;
+}
+</style>
---
+++ src/views/welcome.vue
@@ -0,0 +1,150 @@
+<template>
+    <div class="main">
+        <h1>Welcome back!</h1>
+        <h3>Amount of Letters</h3>
+        <div class="slider_values">
+            <ul>
+                <!-- trying to change the color of the text based off if the number is selected or not -->
+                <li v-bind:class="{'wordNum':true, 'activeWordNum':(this.value === 3)}">3</li>
+                <li v-bind:class="{'wordNum':true, 'activeWordNum':(this.value === 4)}">4</li>
+                <li v-bind:class="{'wordNum':true, 'activeWordNum':(this.value === 5)}">5</li>
+            </ul>
+            </div>
+        <div class="slider">
+            <vue-slider
+            v-model="value"
+            :min="3"
+            :max="5"
+            :interval="1"
+            :disabled="disabled"
+            :tooltip="tooltip"
+            :height="height"
+            :tooltip-placement="tooltipPlacement"
+            :tooltip-style="tooltipStyle"
+            :dot-size="dotSize"
+            :dot-style="dotStyle"
+            :rail-style="railStyle"
+            :process-style="processStyle"
+            ></vue-slider>
+        </div>
+        <h3>Category</h3>
+        <button @click="logValue">Start</button>
+    </div>
+</template>
+
+<script>
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/antd.css'
+
+export default {
+  components: {
+    VueSlider,
+  },
+
+  data() {
+    return {
+      value: 3,
+      disabled: false,
+      tooltip: "none", // 'none' | 'always' | 'hover' | 'focus' | 'active'
+      tooltipPlacement: "bottom", // 'top' | 'right' | 'bottom' | 'left'
+      tooltipStyle: {
+        color: "#97A0FA",
+        backgroundColor: "#97A0FA",
+        border: "2px solid #97A0FA",
+      },
+      height: 20,
+      dotSize: 25,
+      dotStyle: {
+        backgroundColor: "#97A0FA",
+        borderColor: "#f8f8f8",
+      },
+      railStyle: {
+        border: "1px solid #97A0FA",
+        backgroundColor: "#fff",
+      },
+      processStyle: {
+        border: "2px solid #f8f8f8",
+        backgroundColor: "#97A0FA",
+      },
+    };
+  },
+  methods:{
+    logValue(){
+        console.log(this.value);
+    }
+  }
+}
+</script>
+
+<style>
+.main{
+    left: 0;
+    padding: 5vh 0;
+    position: fixed;
+    background: #333333;
+    width: 100%;
+    height: 100%;
+    align-content: center;
+    align-items: center;
+}
+.main h1{
+    padding: 4vh 0px;
+    text-align: center;
+    font-size: 5vh;
+    color: #f8f8f8;
+}
+.main h3{
+    padding: 10px 10px;
+    text-align: center;
+    font-size: 3vh;
+    color: #f8f8f8;
+}
+.main button{
+    padding: 10px 10px;
+    color:#f8f8f8;
+    background: #97A0FA;
+    border-radius: 10px;
+    text-align: center;
+    font-size: 2vh;
+    padding: 10px 30px;
+    border: none;
+    display: inline-block;
+}
+.slider{
+    padding: 10px 10px;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    max-width: 400px;
+}
+
+
+.slider_values ul{
+    list-style: none;
+}
+/* .slider_values li{
+    text-align: center;
+    display: inline-block;
+    padding: 1vh 6vw;
+    color: #f8f8f8;
+    font-size: 2vh;
+} */
+
+.wordNum{
+    text-align: center;
+    display: inline-block;
+    padding: 1vh 6vw;
+    color: #f8f8f8;
+    font-size: 2vh;
+}
+
+.activeWordNum{
+    text-align: center;
+    display: inline-block;
+    padding: 1vh 6vw;
+    color: #97A0FA;
+    font-size: 3vh;
+}
+
+</style>
